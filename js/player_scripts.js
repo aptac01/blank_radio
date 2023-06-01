b25d7546d503eb19a7a69e766c3139efc6867cf8
aptac01 <popovalex402@gmail.com>
2023-06-02 02:15:09 +0700

    хоть какой-то функционал

--- js/player_scripts.js
+++ js/player_scripts.js
@@ -1,31 +1,50 @@
-function hop(name, evt) {
-    console.log('!!!');
-}
-
 function prev_onclick() {
-    alert('prev!');
+    console.log('prev!');
 }
 
 function next_onclick() {
-    alert('next!');
+    console.log('next!');
 }
 
 function play_onclick() {
-    alert('play!');
-    playSound('audio')
+    $('#play_btn').hide();
+    $('#pause_btn').show();
+    playSound('audio');
+    $('#play_indicator').show();
+    $('#play_indicator').removeClass('twinkle');
+}
+
+function pause_onclick() {
+    $('#pause_btn').hide();
+    $('#play_btn').show();
+    ourAudio.pause();
+//    $('#play_indicator').hide();
+    $('#play_indicator').addClass('twinkle');
+
+    // если надо остановить трек
+    // ourAudio.currentTime = 0;
 }
 
 function playSound(url) {
-    var ourAudio = document.createElement('audio'); // Create a audio element using the DOM
-    ourAudio.style.display = "none"; // Hide the audio element
-    ourAudio.src = url; // Set resource to our URL
-    ourAudio.autoplay = true; // Automatically play sound
-    ourAudio.onended = function() {
-        this.remove(); // Remove when played.
-    };
-    document.body.appendChild(ourAudio);
+    var is_playing = $('#current_track').length > 0;
+
+    if (is_playing) {
+        ourAudio.play();
+    } else {
+        ourAudio = document.createElement('audio');
+        ourAudio.id = 'current_track';
+        ourAudio.style.display = "none";
+        ourAudio.src = url;
+        ourAudio.autoplay = true;
+        ourAudio.onended = function() {
+            // Remove when played.
+            this.remove();
+        };
+        document.body.appendChild(ourAudio);
+    }
 }
 
 window.onload = (function (){
     $('#player').load('player_svg');
+    var ourAudio;
 });
